40300390b67352d98b7d6f499e0a74ffab841dd4
Gokhan Gokalp <gokhan.gokalp@trendyol.com>
2017-05-24 16:49:13 +0300

    Initial commit

---
+++ Httwrap.RequestCorrelationInterceptor/RequestCorrelationInterceptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using Httwrap.Interception;
+
+namespace Httwrap.RequestCorrelationInterceptor
+{
+    public class RequestCorrelationInterceptor : IHttpInterceptor
+    {
+        public void OnRequest(HttpRequestMessage request)
+        {
+            if (System.Diagnostics.Trace.CorrelationManager.ActivityId != Guid.Empty)
+            {
+                request.Headers.Add("correlationId", System.Diagnostics.Trace.CorrelationManager.ActivityId.ToString());
+            }
+        }
+
+        public void OnResponse(HttpRequestMessage request, HttpResponseMessage response)
+        {
+        }
+    }
+}
